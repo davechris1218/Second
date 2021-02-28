c1f300f9375651f8e80ef1d10df1c3a4571044a2
davechris1218 <74490003+davechris1218@users.noreply.github.com>
2021-02-28 21:57:32 +0700

    piramida

--- piramida.php
+++ piramida.php
@@ -4,31 +4,25 @@
 <body>
       <?php
 
-      $star = 5;
+      echo "<pre>";
       echo "Input tinggi segitiga: 5" . "<br>" . "<br>";
-      for ($x = $star; $x > 0; $x--) {
-            for ($i = 1; $i <= $x; $i++) {
-                  echo "&nbsp";
-            }
-            for ($y = $star; $y >= $x; $y--) {
-                  echo "*";
-            }
-            echo "<br>";
+      for ($i = 1; $i < 5; $i++) {
+            $space = str_repeat(" ", 5 - $i);
+            $star = str_repeat("*", ($i - 1) * 2 + 1);
+            echo $space . $star . "<br>";
       }
+      echo "</pre>";
 
       echo "<br>";
-
-      $star = 7;
+      
+      echo "<pre>";
       echo "Input tinggi segitiga: 7" . "<br>" . "<br>";
-      for ($x = $star; $x > 0; $x--) {
-            for ($i = 1; $i <= $x; $i++) {
-                  echo "&nbsp";
-            }
-            for ($y = $star; $y >= $x; $y--) {
-                  echo "*";
-            }
-            echo "<br>";
+      for ($i = 1; $i < 7; $i++) {
+            $space = str_repeat(" ", 7 - $i);
+            $star = str_repeat("*", ($i - 1) * 2 + 1);
+            echo $space . $star . "<br>";
       }
+      echo "</pre>";
 
       ?>
 
