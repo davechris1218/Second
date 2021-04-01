94965572dfcfd9a163b2d6810fbb92106a13cb86
davechris1218 <74490003+davechris1218@users.noreply.github.com>
2021-04-01 23:52:40 +0700

    login & signup form

--- registration/server.php
+++ registration/server.php
@@ -1,53 +1,80 @@
 <?php
-session_start();
+    session_start();
 
-$username = "";
-$email = "";
-$errors = array();
+    $username = "";
+    $email = "";
+    $errors = array();
 
-$db = mysqli_connect('localhost', 'nagax21', 'Fiorenasitalia1234', 'registration');
+    $db = mysqli_connect('localhost', 'nagax21', 'Fiorenasitalia1234', 'registration');
 
-if (isset($_POST['reg_user'])) {
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $email = mysqli_real_escape_string($db, $_POST['email']);
-    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-}
+    if (isset($_POST['reg_user'])) {
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $email = mysqli_real_escape_string($db, $_POST['email']);
+        $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+        $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+    
 
-if (empty($username)) {
-    array_push($errors, "Username is required");
-}
+        if (empty($username)) {
+            array_push($errors, "Username is required");
+        }
 
-if (empty($email)) {
-    array_push($errors, "Email is required");
-}
+        if (empty($email)) {
+            array_push($errors, "Email is required");
+        }
 
-if (empty($password_1)) {
-    array_push($errors, "Password is required");
-}
+        if (empty($password_1)) {
+            array_push($errors, "Password is required");
+        }
 
-if (empty($password_2)) {
-    array_push($errors, "Password does not match");
-}
+        if ($password_1 != $password_2) {
+            array_push($errors, "The passwords do not match");
+        }
 
-$user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-$result = mysqli_query($db, $user_check_query);
-$user = mysqli_fetch_assoc($result);
+        $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+        $result = mysqli_query($db, $user_check_query);
+        $user = mysqli_fetch_assoc($result);
 
-if ($user) {
-    if ($user['username'] === $username) {
-        array_push($errors, "Username already exists");
+        if ($user) {
+            if ($user['username'] === $username) {
+                array_push($errors, "Username already exists");
+            }
+            if ($user['email'] === $email) {
+                array_push($errors, "Email already exists");
+            }
+        }
+
+        if (count($errors) == 0) {
+            $password = md5($password_1);
+            $query = "INSERT INTO users (username, email, password) VALUES('$username', '$email', '$password')";
+            mysqli_query($db, $query);
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            header('location: index_2.php');
+        }
     }
-    if ($user['email'] === $email) {
-        array_push($errors, "Email already exists");
-    }
-}
 
-if (count($errors) == 0) {
-    $password = md5($password_1);
-    $query = "INSERT INTO users (username, email, password) VALUES('$username', '$email', '$password')";
-    mysqli_query($db, $query);
-    $_SESSION['username'] = $username;
-    $_SESSION['success'] = "You are now logged in";
-    header('location: index.php');
-}
+    if (isset($_POST['login_user'])) {
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+    
+        if (empty($username)) {
+            array_push($errors, "Username is required");
+        }
+        if (empty($password)) {
+            array_push($errors, "Password is required");
+        }
+       
+        if (count($errors) == 0) {
+            $password = md5($password);
+            $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+            $results = mysqli_query($db, $query);
+            if (mysqli_num_rows($results) == 1) {
+                $_SESSION['username'] = $username;
+                $_SESSION['success'] = "You are now logged in";
+                header('location: index_2.php');
+            }else {
+                array_push($errors, "Wrong username/password combination");
+            }
+        }
+    }
+?>
